b5489520852ba66a63553f8c8641d0ce830316f2
EricPhanHuy <huydinhphan2001@gmail.com>
2026-02-25 10:02:05 +0700

    added Navbar and HeroSection

--- src/app/(nondashboard)/landing/layout.tsx
+++ src/app/(nondashboard)/landing/layout.tsx
@@ -1,11 +1,16 @@
+import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
+import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      {/* <Navbar /> */}
+    <div className="h-full w-full">
+      <Navbar />
       navbar
-      <main className={`h-full flex w-full flex-col pt-[${NAVBAR_HEIGHT}px]`}>
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
         {children}
       </main>
     </div>
