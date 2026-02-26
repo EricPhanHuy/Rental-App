5e1436126f2f1cd433fb444ddb964e6f738d9719
EricPhanHuy <huydinhphan2001@gmail.com>
2026-02-26 11:57:38 +0700

    add FeatureSection DiscoverSection CalltoActionSection

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+import CalltoActionSection from "./CalltoActionSection";
 
 const Landing = () => {
   return (
     <div>
       <HeroSection />
       <FeaturesSection />
+      <DiscoverSection />
+      <CalltoActionSection />
     </div>
   );
 };
